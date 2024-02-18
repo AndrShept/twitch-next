616c82f83f5676f99c91adddca360d34ee55afe4
lolokos86 <lolokos1986@gmail.com>
2024-02-18 16:49:16 +0200

    Changes to be committed: 	modified:   src/app/(main)/page.tsx 	new file:   src/app/api/webhooks/livekit/route.ts 	modified:   src/middleware.ts

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,9 +2,9 @@ import { createUser } from '@/lib/services/user-service';
 import { currentUser } from '@clerk/nextjs';
 
 export default async function Home() {
-  const profile = await currentUser();
-  if (profile) {
-   await createUser();
-  }
+  // const profile = await currentUser();
+  // if (profile) {
+  //  await createUser();
+  // }
   return <>Home</>;
 }
