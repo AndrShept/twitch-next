462557125f754e56ebea900c81332ed79e05584c
lolokos86 <lolokos1986@gmail.com>
2024-03-02 15:02:24 +0200

    Changes to be committed: 	modified:   src/app/(main)/page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,25 +1,7 @@
-import { prisma } from '@/lib/db/prisma';
-import { createUser } from '@/lib/services/user-service';
-import { currentUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
-import { userAgent } from 'next/server';
-import { Suspense } from 'react';
 
-import { StreamList, StreamListSkeleton } from './_components/StreamList';
+import { StreamList } from './_components/StreamList';
 
 export default async function Home() {
-  // const self = await currentUser();
-  // // if (!self) {
-  // //   redirect('/sign-up');
-  // // }
-  // const user = await prisma.user.findFirst({
-  //   where: { externalUserId: self?.id },
-  // });
-  // if (!user) {
-  //   const res = await createUser();
-  //   console.log(res);
-  //   console.log('GO!!!');
-  // }
 
 
 
